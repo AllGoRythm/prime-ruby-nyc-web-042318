84c76964fad146b9d0908f224909df9de9c649fe
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-13 18:33:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,27 @@
   def prime?(num)
     if num <= 1
       return false
-
     elsif num == 2
       return true
-
     else
+
+num.each do |check == 2|
+  num % check == 0
+  return false
+end
+
+
+=begin
     i = 2
+
       while i < num
         if num % i == 0
           return false
         end
+
         i += 1
       end
+=end
     end
 
     return true
