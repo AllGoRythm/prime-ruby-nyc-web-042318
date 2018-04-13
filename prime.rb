b933bdfaaa115ff25158903bc94644ef75be1bf3
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-13 18:11:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,13 @@
-def prime?(num)
-  
+def prime?(max)
+  primes = []
+
+  (2...max).each do |num|
+    is_prime = true
+    (2...num).each do |divisor|
+      is_prime = false if (num % divisor == 0)
+    end
+    primes << num if is_prime
+  end
+
+  primes
 end
